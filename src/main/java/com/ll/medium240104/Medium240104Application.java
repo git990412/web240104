6a3f8fa4f2a1a38695274f03b9df8690fcfea136
git990412 <wjdwn282@gmail.com>
2024-01-04 22:40:01 +0900

    start

---
+++ src/main/java/com/ll/medium240104/Medium240104Application.java
@@ -0,0 +1,13 @@
+package com.ll.medium240104;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medium240104Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Medium240104Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/medium240104/Medium240104ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.medium240104;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Medium240104ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
